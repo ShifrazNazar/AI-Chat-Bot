dc52e8f34438db025bea240f76ce9fe961217ffa
Shifraz Nazar <shifraznazar@gmail.com>
2023-11-26 23:53:08 +0800

    Update ChatApp.jsx

--- src/components/ChatApp.jsx
+++ src/components/ChatApp.jsx
@@ -1,8 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Configuration, OpenAIApi } from "openai";
 import Navbar from "./Navbar";
-import { collection, addDoc, getDocs } from "firebase/firestore";
-import { db } from "../firebase";
 
 const ChatApp = () => {
   const configuration = new Configuration({
@@ -13,11 +11,6 @@ const ChatApp = () => {
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState("");
 
-  useEffect(() => {
-    // Fetch chat history on component mount
-    fetchChatHistory();
-  }, []);
-
   const addMessage = (text, isUser = false) => {
     setMessages((prevMessages) => [
       ...prevMessages,
@@ -27,12 +20,15 @@ const ChatApp = () => {
 
   const getBotResponse = async (userInput) => {
     try {
+      const prompt = `As a Google software engineer, how would you respond to the following:\n\n${userInput}\n\nGoogle Engineer:`;
+
       const response = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: userInput,
+        prompt: prompt,
         max_tokens: 100,
         temperature: 0.9,
       });
+
       const botResponse = response.data.choices[0].text.trim();
       addMessage(botResponse);
     } catch (error) {
@@ -48,54 +44,44 @@ const ChatApp = () => {
     setInput("");
   };
 
-
-  const fetchChatHistory = async () => {
-    const querySnapshot = await getDocs(collection(db, "chatHistory"));
-    setSavedChats(querySnapshot.docs.map((doc) => doc.data()));
-  };
-
-
-
   return (
     <div className="bg-gray-900 h-screen">
       <Navbar />
-        
-      <div className="px-20 ">
 
-          <h1 className="text-4xl text-gray-100 text-center mt-4">
-            Chat with AI
-          </h1>
-          <div className="bg-gray-800 rounded-md p-4 mt-4 h-80 overflow-y-auto">
-            {messages.map((message) => (
-              <div
-                key={message.id}
-                className={`px-4 py-2 rounded-md ${
-                  message.isUser
-                    ? "bg-blue-600 text-gray-100"
-                    : "bg-gray-700 text-gray-300"
-                }`}
-              >
-                {message.text}
-              </div>
-            ))}
-          </div>
-          <form onSubmit={handleSubmit} className="mt-4">
-            <textarea
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-              className="bg-gray-700 text-gray-100 px-3 py-2 rounded-md w-full"
-              placeholder="Type your message..."
-            />
-            <button
-              type="submit"
-              className="bg-blue-600 text-gray-100 px-4 py-2 rounded-md mt-4"
+      <div className="px-20 ">
+        <h1 className="text-4xl text-gray-100 text-center mt-4">
+          Chat with a Google Engineer
+        </h1>
+        <div className="bg-gray-800 rounded-md p-4 mt-4 h-80 overflow-y-auto">
+          {messages.map((message) => (
+            <div
+              key={message.id}
+              className={`px-4 py-2 rounded-md ${
+                message.isUser
+                  ? "bg-blue-600 text-gray-100"
+                  : "bg-gray-700 text-gray-300"
+              }`}
             >
-              Submit
-            </button>
-    
-          </form>
+              {message.text}
+            </div>
+          ))}
         </div>
+        <form onSubmit={handleSubmit} className="mt-4">
+          <textarea
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className="bg-gray-700 text-gray-100 px-3 py-2 rounded-md w-full"
+            placeholder="Type your message..."
+          />
+          <button
+            type="submit"
+            className="bg-blue-600 text-gray-100 px-4 py-2 rounded-md mt-4"
+          >
+            Submit
+          </button>
+        </form>
       </div>
+    </div>
   );
 };
 
